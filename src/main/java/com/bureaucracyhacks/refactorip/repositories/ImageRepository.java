eaa6a33e89ed72ad10ecc7bcc8e8bf94d841ccd6
Lucian <lucian_munteanu@mail.ru>
2023-05-26 18:13:40 +0300

    release

---
+++ src/main/java/com/bureaucracyhacks/refactorip/repositories/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.bureaucracyhacks.refactorip.repositories;
+
+import java.util.Optional;
+
+import com.bureaucracyhacks.refactorip.models.DocumentImageJPA;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ImageRepository extends JpaRepository<DocumentImageJPA, Long>{
+    Optional<DocumentImageJPA> findByName(String imageName);
+    @Query(value="SELECT * FROM doc_image i WHERE i.name = ?1 AND i.id_user = ?2", nativeQuery = true)
+    Optional<DocumentImageJPA> findByNameOnUser(String imageName, int idUser);
+
+    @Query(value="SELECT * FROM doc_image i WHERE i.id_user = ?1", nativeQuery = true)
+    Iterable<DocumentImageJPA> findAllByUserId(int idUser);
+}
